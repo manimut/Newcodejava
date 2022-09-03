ad1da4211db772d8d5c914aaef5a160ae84561dc
manimut <manimuthu3366@gmail.com>
2022-09-03 08:16:15 +0530

    myname

---
+++ src/org/test/EvenOdd.java
@@ -0,0 +1,14 @@
+package org.test;
+
+public class EvenOdd {
+	public static void main(String[] args) {
+		int a=12;
+		if(a%2==0) {
+			System.out.println(" is Even number");
+		}
+		else {
+			System.out.println(" is Odd number");
+		}
+	}
+
+}
---
+++ src/org/test/Hcs.java
@@ -0,0 +1,23 @@
+package org.test;
+
+public class Hcs   {
+	
+public void mani() {
+	
+	System.out.println("main");
+
+
+}
+public void bharath() {
+	System.out.println("bharathy");
+
+
+}
+
+		
+}
+
+
+	
+
+
